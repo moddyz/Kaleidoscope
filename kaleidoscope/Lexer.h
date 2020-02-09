6e31a5fcf24e42bac9f02aedcb02062d45813611
moddyz <moddy3d@gmail.com>
2020-02-09 18:43:45 +1300

    [Code Format] Added .clang-format and ran over existing c++ code

--- kaleidoscope/Lexer.h
+++ kaleidoscope/Lexer.h
@@ -4,20 +4,19 @@
 
 namespace kaleidoscope
 {
-
 /// Token describes the type of token.
 enum Token
 {
     // End of file.
     Token_Eof = -1,
-    
+
     // Commands.
-    Token_Def = -2,
+    Token_Def    = -2,
     Token_Extern = -3,
 
     // Primary.
     Token_Identifier = -4,
-    Token_Number = -5
+    Token_Number     = -5
 };
 
 /// Return the next Token from standard input.
@@ -29,4 +28,4 @@ static const std::string& LexerGetIdentifierValue();
 /// Get the value of the last parsed Number token.
 static double LexerGetNumberValue();
 
-}
+} // namespace kaleidoscope
