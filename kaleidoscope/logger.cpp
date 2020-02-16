a8011a96a07ec1a6e724d06066d089619edd5c95
moddyz <moddy3d@gmail.com>
2020-02-16 18:57:59 +1300

    Added LogInfo

--- kaleidoscope/logger.cpp
+++ kaleidoscope/logger.cpp
@@ -3,15 +3,28 @@
 
 namespace kaleidoscope
 {
-void LogError( const char* i_errorMessage, ... )
+/// Utility method for logging to a standard stream.
+void Log( FILE* io_stream, const char* i_messageFormat, const char* i_message, va_list i_args )
 {
     constexpr size_t bufferSize = 256;
     char             buffer[ bufferSize ];
-    va_list          args;
-    va_start( args, i_errorMessage );
-    vsnprintf( buffer, bufferSize, i_errorMessage, args );
-    va_end( args );
-    fprintf( stderr, "Error: %s\n", buffer );
+    vsnprintf( buffer, bufferSize, i_message, i_args );
+    fprintf( io_stream, i_messageFormat, buffer );
 }
 
+void LogError( const char* i_errorMessage, ... )
+{
+    va_list args;
+    va_start( args, i_errorMessage );
+    Log( stderr, "[ERROR] %s\n", i_errorMessage, args );
+    va_end( args );
+}
+
+void LogInfo( const char* i_message, ... )
+{
+    va_list args;
+    va_start( args, i_message );
+    Log( stdout, "[INFO] %s\n", i_message, args );
+    va_end( args );
+}
 } // namespace kaleidoscope
--- kaleidoscope/logger.h
+++ kaleidoscope/logger.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <cstdio>
+#include <stdarg.h>
+#include <utility>
+
 namespace kaleidoscope
 {
 /// Log an error message.
 /// \param i_errorMessage message to log as an error.
 void LogError( const char* i_errorMessage, ... );
+
+/// Log an informative message.
+/// \param i_message mesage log as an information.
+void LogInfo( const char* i_message, ... );
 
 } // namespace kaleidoscope
